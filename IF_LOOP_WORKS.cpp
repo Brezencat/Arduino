8c89329835a602c4a092fc211ada407178d02269
Brezencat <brezencat@gmail.com>
2020-06-29 22:19:30 +0300

    для понимания цикла и условия если

---
+++ IF_LOOP_WORKS.cpp
@@ -0,0 +1,20 @@
+//pin7 = red
+//pin8 = green
+wait = 1000;
+
+void loop()
+(
+    digitalWrite(red, HIGH);
+    delay(wait);
+    digitalWrite(red, LOW);
+    delay(wait);
+    counter++;
+
+    if(counter == 5)
+    (
+        digitalWrite(green, HIGH);
+        delay(3000);
+        digitalWrite(green, LOW);
+        counter = 0;
+    )
+)
